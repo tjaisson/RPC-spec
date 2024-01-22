1397aa6eab3b99b958543fc35970c4a441b0ed42
tjaisson <thomas.jaisson@ac-paris.fr>
2024-01-22 20:52:35 +0000

    More generic contract

--- src/contract/index.ts
+++ src/contract/index.ts
@@ -2,13 +2,27 @@
  * Un contrat d'interface est un ensemble de fonctions.
  */
 export type ContractInterface = {
-    [key: string]: (...args: any) => any;
-};
+  [key: string]: (...args: unknown[]) => unknown;
+}
 
 /**
  * Un contrat spécifie l'interface exposée par chaque partie.
  */
-export type Contract = {
+export type UnknownContract = {
   metaplayer: ContractInterface;
   application: ContractInterface;
+}
+
+import type { Contract as BasicContract } from './basic';
+
+type ContractMap = {
+  '/': UnknownContract;
+  '/basic(string)': BasicContract<string>;
+}
+
+export type ContractId = keyof ContractMap;
+
+type Identify<ID extends string, C extends UnknownContract> = C & { id: ID; };
+export type Contract = {
+  [C in ContractId]: Identify<C, ContractMap[C]>;
 }
